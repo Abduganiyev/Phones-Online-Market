e09a50e0b5ce75c3aa416f97df1fa263ca502489
Abduganiyev <mr.show050@gmail.com>
2022-06-24 20:06:28 +0500

    initial commit

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,18 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     private Long id;
-    private Long chatId;
+    private Long chat_id;
     private String firstname;
+    private boolean isBot;
     private String lastname;
     private String username;
     private String phoneNumber;
     private BotState botState;
+    private Long user_roles;
     private String created_at;
 }
---
+++ src/main/java/repository/implement/RoleRepositoryImp.java
@@ -0,0 +1,2 @@
+package repository.implement;public class RoleRepositoryImp {
+}
